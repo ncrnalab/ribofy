2c3ebea0f9a211104ea7733439496b29f327906e
ncrnalab <tbh@mbg.au.dk>
2021-07-07 14:50:29 +0200

    Support multiple bam

--- ribofy/get_offset.py
+++ ribofy/get_offset.py
@@ -12,156 +12,174 @@ And change number of ORFs used in offset calculation by the --norfs option
 
 """
 
-
-import sys
 import pysam
 import pandas as pd
 import numpy as np
+from collections import Counter
 from .argparse2 import argparse2
 from .get_phasing import get_phasing_stats, get_phasing_matrix
 from .bam_utils import get_tid_info
 
 
-def percentile(n):
+def agg_percentile(n, postfix = ""):
     def percentile_(x):
         return x.quantile(n)
-    percentile_.__name__ = 'percentile_{:2.0f}'.format(n*100)
+    percentile_.__name__ = ('percentile_' + postfix).strip ("_")
     return percentile_
 
-def get_offset (bamfile, orfs, output, norfs=10, min_read_length=25, max_read_length=35):
+def agg_table(ret = "key"): #key/value/pct
+    def table_(x):
+        ctr = Counter(x).most_common()
+        if ret == "key":            
+            return ([k for (k,v) in ctr][0])
+        elif ret == "value":
+            return ([v for (k,v) in ctr][0])
+        elif ret == "pct":
+            return ([v/len(x) for (k,v) in ctr][0])
+
+
+    table_.__name__ = f"table_{ret}"
+    return table_
+
+
+def get_offset (bamfiles, orfs, output, norfs=20, min_read_length=25, max_read_length=35, percentile = .9, devel=False):
 
     print ("### get_offset ###")
-
     print ("loading orfs...")
 
     pd_orfs = pd.read_csv (orfs, sep="\t")
-    pd_orfs = pd_orfs[(pd_orfs.orf_type == "annotated")] \
+
+    pd_annot = pd_orfs[(pd_orfs.orf_type == "annotated") & (pd_orfs.orf_length >= 500)] \
         .groupby ("orf_group") \
         .head (1) 
 
-    # get transcripts with most counts
-    print ("get transcript counts from bam...")
+    pd_combined = pd.DataFrame ()
+    pd_devel = pd.DataFrame ()
 
-    # load bam
-    bam = pysam.Samfile (bamfile)
-    
-    dtid2count, dtid2ref = get_tid_info (bamfile)
+    for bamfile in bamfiles:
 
-    
-    # add read counts to dataframe
-    pd_orfs['total_reads'] = pd_orfs['tid'].transform (lambda x: dtid2count[x])
-    pd_orfs = pd_orfs.sort_values ('total_reads', ascending=False)
+        # get transcripts with most counts
+        print ("get transcript counts from bam...")
 
-    print ("using the following transcripts..:")
-    print (pd_orfs[['tid', 'total_reads']].head (norfs))
+        # load bam
+        bam = pysam.Samfile (bamfile)
+        
+        dtid2count, dtid2ref = get_tid_info (bamfile)
+        
+        # add read counts to dataframe
+        pd_annot['total_reads'] = pd_annot['tid'].transform (lambda x: dtid2count[x] if x in dtid2count else 0)
+        
+        pd_annot = pd_annot.sort_values ('total_reads', ascending=False)
+
+        print ("determining p-site offsets using the following transcripts..:")
+        print (pd_annot[['tid', 'total_reads']].head (norfs))
 
 
-    # initialize count_offsets
-    length_range = range (min_read_length, max_read_length+1) 
-    
-    count_offsets = {}
+        # initialize count_offsets
+        length_range = range (min_read_length, max_read_length+1) 
+        
+        count_offsets = {}
 
-    for x in length_range:
-        count_offsets[x] = [0,0,0]
+        for x in length_range:
+            count_offsets[x] = [0,0,0]
 
-    off_conv = {0:0, 1:2, 2:1}
+        off_conv = {0:0, 1:2, 2:1}
 
-    offset_stats = []
+        offset_stats = []
 
-    for i, row in pd_orfs.head (norfs).iterrows ():
+        for i, row in pd_annot.head (norfs).iterrows ():
 
-        tid, start, end = dtid2ref[row['tid']], int(row['start']), int(row['stop'])+3
+            tid, start, end = dtid2ref[row['tid']], int(row['start']), int(row['stop'])+3
 
-        dcds = {}
-        for lr in length_range:
-            dcds[lr] = [0] * (end-start)
-                
-        for read in bam.fetch (tid, start, end):
+            dcds = {}
+            for lr in length_range:
+                dcds[lr] = [0] * (end-start)
+                    
+            for read in bam.fetch (tid, start, end):
 
-            init_offset_pos = read.pos + 12
+                init_offset_pos = read.pos + 12
 
-            read_length = read.infer_read_length () 
+                read_length = read.infer_read_length () 
 
-            if read_length < min_read_length or read_length > max_read_length:
-                continue
+                if read_length < min_read_length or read_length > max_read_length:
+                    continue
 
-            if init_offset_pos >= start and init_offset_pos < end: 
+                if init_offset_pos >= start and init_offset_pos < end: 
 
-                init_rel_pos = init_offset_pos - start            
-                offset = off_conv[init_rel_pos % 3]
-                
-                rel_pos = (12 + offset + read.pos) - start
+                    init_rel_pos = init_offset_pos - start            
+                    offset = off_conv[init_rel_pos % 3]
+                    
+                    rel_pos = (12 + offset + read.pos) - start
 
-                if rel_pos % 3 != 0:
-                    print ("something wrong with offset")
-       
-                count_offsets[read_length][offset] += 1
-                
-                if init_rel_pos >= 0 and init_rel_pos < len (dcds[read_length]): 
-                    dcds[read_length][init_rel_pos] += 1
+                    if rel_pos % 3 != 0:
+                        print ("something wrong with offset")
+        
+                    count_offsets[read_length][offset] += 1
+                    
+                    if init_rel_pos >= 0 and init_rel_pos < len (dcds[read_length]): 
+                        dcds[read_length][init_rel_pos] += 1
 
-        #print (dcds)
-        for lr in length_range:
             
-            mat = get_phasing_matrix (dcds[lr])
+            # check phasing for each read length individually
+            for lr in length_range:
+                
+                mat = get_phasing_matrix (dcds[lr])
 
-            frame_sort = np.argsort(mat.sum(axis=0))[::-1]
+                frame_sort = np.argsort(mat.sum(axis=0))[::-1]
 
-            mat = mat[:,frame_sort]
+                # set the frame with most count as onframe (index 0)
+                mat = mat[:,frame_sort]
 
-            wilcox_p, binom_p = get_phasing_stats (mat)
+                # test whether onframe counts are significantly enriched across CDS
+                wilcox_p, binom_p, glm_p = get_phasing_stats (mat)
 
-            offset_stats.append ({
-                'read_length' : lr,
-                'tid' : tid,
-                'p_wilcox' : wilcox_p,
-                'p_binom' : binom_p,
-                'offset' : 12 + off_conv[frame_sort[0]],
-                'onframe' : np.sum (mat[:,0]),
-                'offframe1' : np.sum (mat[:,1]),
-                'offframe2' : np.sum (mat[:,2])                
-            })
-
-    pd_stats = pd.DataFrame (offset_stats)
-    
-    pd_agg = pd_stats.dropna().groupby ('read_length').agg ({'p_wilcox' : [percentile (.1), percentile (.9)],
-                                          'p_binom' : [percentile (.1), percentile (.9)],
-                                          'offset' : [percentile (.1), percentile (.9)],
-                                          'onframe' : [np.sum],
-                                          'offframe1' : [np.sum],
-                                          'offframe2' : [np.sum]})
-
-    pd_agg['offset'] = pd_agg['offset'].astype ("int")
-    pd_agg['mean_offset'] = pd_agg['offset'].mean (axis=1)
-
-    pd_agg.columns = ['_'.join(col).strip("_") for col in pd_agg.columns.values]
-    
-    pd_agg['bam'] = bamfile
-
-    print (pd_agg)
-
-    pd_agg.to_csv ("pd_stats.txt", sep="\t")
-    
-    
-    # # compile dataframe
-    # pd_offsets = pd.DataFrame ({        
-    #     'read_length' : list (length_range),
-    #     'offset' : [str(12+count_offsets[x].index (max(count_offsets[x]))) for x in length_range],
-    #     'reads12' : [count_offsets[x][0] for x in length_range],
-    #     'reads13' : [count_offsets[x][1] for x in length_range],
-    #     'reads14' : [count_offsets[x][2] for x in length_range],
-    #     'bam' : bamfile
-    # })
+                offset_stats.append ({
+                    'read_length' : lr,
+                    'tid' : tid,
+                    'offset' : 12 + off_conv[frame_sort[0]], # best offset
+                    'p_wilcox' : wilcox_p,  # stats for best offset
+                    'p_binom' : binom_p,   # stats for best offset
+                    'p_glm' : glm_p,   # stats for best offset
+                    'onframe' : np.sum (mat[:,0]),  # sum of reads with onframe psites
+                    'offframe1' : np.sum (mat[:,1]),
+                    'offframe2' : np.sum (mat[:,2])                
+                })
 
 
-    pd_agg.to_csv (output, sep="\t")
+        pd_stats = pd.DataFrame (offset_stats)
 
-    print (pd_agg.columns)
+        if devel:
+            pd_devel = pd.concat ([pd_devel,pd_stats])
+            
+        # for each read-length aggregate the results for the analysed transcripts
+        pd_stats = pd_stats \
+            .dropna().groupby ('read_length') \
+            .agg ({'p_wilcox' : [agg_percentile (percentile)],
+                'p_binom' : [agg_percentile (percentile)],
+                'offset' : [agg_table("key"), agg_table("value"), agg_table("pct")], # [percentile (.05), percentile (.95)],
+                'onframe' : [np.sum],
+                'offframe1' : [np.sum],
+                'offframe2' : [np.sum]})
+
+        pd_stats.columns = ['_'.join(col).strip("_") for col in pd_stats.columns.values]
+        
+        pd_stats['bam'] = bamfile
+
+        pd_combined = pd.concat ([pd_combined, pd_stats])
+
 
     print ("extracted offsets:")
-    print (pd_agg[['mean_offset']])
-    print ("done")
+    print (pd_combined[['offset_table_key', 'offset_table_pct']])
 
+    # save to output
+    pd_combined.to_csv (output, sep="\t")
+
+    if devel:
+        pd_devel.to_csv (output + "_devel.txt", sep="\t")
+
+    print ("### Done ###")
+    
+   
 
 def ribofy_offset ():
 
@@ -174,21 +192,23 @@ def ribofy_offset ():
     parser.add_argument('detect', nargs='?', help='') # dummy argument
     parser._action_groups.pop()
 
-    parser.add_argument("--bam",   dest='bam', required=True, help="bam file - sorted and indexed")
+    parser.add_argument("--bam",   dest='bam', nargs="+", required=True, help="bam file - sorted and indexed")
     parser.add_argument("--orfs",  dest='orfs', required=True, help="orfs - generated by get_ORFs.py")
     parser.add_argument("--output", dest='output', default = "ribofy_offsets.txt", help="output")
     
     #optional
-    parser.add_argument('--norfs', dest='norfs', default = 10, type = int, help="number of distinct orfs to build offsets")
+    parser.add_argument('--norfs', dest='norfs', default = 20, type = int, help="number of distinct orfs to build offsets")
     parser.add_argument('--min_read_length', dest='min_read_length', default = 25, type = int, help="minimum read length used in analysis")
     parser.add_argument('--max_read_length', dest='max_read_length', default = 35, type = int, help="maximum read length used in analysis")
+    parser.add_argument("--percentile", dest='percentile', default = 0.9, help="Percentile of consistent offset-determinants")
     
     args = parser.parse_args()
 
     get_offset (args.bam, args.orfs, args.output, 
                 norfs=args.norfs, 
                 min_read_length = args.min_read_length,
-                max_read_length = args.max_read_length)
+                max_read_length = args.max_read_length,
+                percentile=args.percentile)
 
 
 if __name__ == "__main__":
--- ribofy/get_phasing.py
+++ ribofy/get_phasing.py
@@ -6,24 +6,31 @@ python get_phasing.py --bam <bam-file> --orfs <ribofy orfs-file> --offsets <ribo
 
 """
 
-
-import sys
 import argparse
 import pysam
 import pandas as pd
 import numpy as np
+import warnings
 from tqdm import tqdm
-from scipy.stats import wilcoxon, binomtest
+from scipy.stats import wilcoxon, binomtest, f
+import statsmodels.formula.api as smf 
+import statsmodels.api as sm
+import statsmodels.tools.sm_exceptions as sme
 from .bam_utils import get_tid_info
 
-
-
+libmtspec = True
+try:
+    from mtspec import mtspec
+except ModuleNotFoundError:
+    libmtspec = False
+    
 def get_phasing_matrix (cds):
     
     mat = np.array ([[s for i, s in enumerate (cds) if i%3 == 0], # on-frame
                      [s for i, s in enumerate (cds) if i%3 == 1],
                      [s for i, s in enumerate (cds) if i%3 == 2]]).T
 
+    
     return (mat)
 
 
@@ -32,34 +39,154 @@ def get_phasing_stats (mat):
     # remove codons without any signal
     mat = mat[~np.all(mat == 0, axis=1)]
 
-    # wilcoxon-test for frame0 > mean (frame1, frame2)
-    # diff = mat[:,0] - np.mean (mat[:,1:3], axis=1)
-    # non_zero = np.sum(diff != 0)
-    #mode = "exact" if non_zero <= 25 else "auto"            
-    wilcox_stat, wilcox_p = wilcoxon(mat[:,0], np.mean (mat[:,1:3], axis=1), alternative="greater") if mat.shape[0] >= 10 else (np.nan, np.nan)
+    if mat.shape[0] < 10:
+        return (np.nan, np.nan, np.nan)
 
-    # binomial-test for n(frame0 > frame1 and frame0 > frame2)
-    # add random noise to account for draw-bias
-    mat = mat + np.random.uniform(low=0.0, high=0.99, size=mat.shape)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
 
-    index_max = np.argmax (mat, axis=1)
-    binom_p = binomtest (k=np.sum (index_max == 0), n=len(index_max), p=1/3, alternative="greater").pvalue if len (index_max) > 0 else np.nan
+        # nb glm
+        
+        df_glm = pd.DataFrame ({
+            'counts' : mat.reshape (-1),
+            'frame' : ['f1', 'f2', 'f3'] * mat.shape[0]
+        })
 
-    return (wilcox_p, binom_p)
+        try:
+            model = smf.glm(formula = "counts ~ frame", data=df_glm, family=sm.families.NegativeBinomial()).fit()
+            r = np.array([1,-.5, -.5])
+            glm_ttest = model.t_test(r)
+            glm_p = glm_ttest.pvalue
+        except sme.PerfectSeparationError:
+            glm_p = np.nan
 
-def get_phasing (bamfile, orfs, offsets, output):
+
+        # wilcoxon-test for frame0 > mean (frame1, frame2)
+        wilcox_stat, wilcox_p = wilcoxon(mat[:,0], np.mean (mat[:,1:3], axis=1), alternative="greater") if mat.shape[0] >= 10 else (np.nan, np.nan)
+
+        # binomial-test for n(frame0 > frame1 and frame0 > frame2)
+        # add random noise to reduce draw-bias
+        mat = mat + np.random.uniform(low=0.0, high=0.99, size=mat.shape)
+
+        index_max = np.argmax (mat, axis=1)
+        binom_p = binomtest (k=np.sum (index_max == 0), n=len(index_max), p=1/3, alternative="greater").pvalue if len (index_max) > 0 else np.nan
+
+    return (wilcox_p, binom_p, glm_p)
+
+def get_spectrum (cds, time_bandwidth = 3, ntapers = "default", nfft = "default"):
+    
+    if sum (cds) == 0:
+        return (np.nan)
+
+    if nfft == "default":
+        nfft = int(2 * 2**np.ceil(np.log2(len(cds))))
+
+    if ntapers == "default":
+        ntapers = int(2*time_bandwidth) - 1
+
+    # Calculate the spectral estimation.
+    spec, freq, jackknife, fstatistics, _ = mtspec(data=np.array(cds), delta = 1, time_bandwidth = time_bandwidth, number_of_tapers=ntapers, nfft=nfft, statistics=True, rshape=0)
+
+    m = int(np.round (nfft/3))
+    sf = f.sf (fstatistics[m],dfn=2,dfd=(2*ntapers)-2)
+    return (sf)
+
+
+def get_psites (orf_tid, start, stop, bamfiles, pd_offsets, tid2ref_dict = None, bam_dict = None):
+
+    if tid2ref_dict == None or bam_dict == None:
+        
+        tid2ref_dict, bam_dict = {},{}
+        for bamfile in bamfiles:
+
+            bam_dict[bamfile] = pysam.Samfile (bamfile)
+
+            _, tid2ref = get_tid_info (bamfile)
+            tid2ref_dict[bamfile] = tid2ref
+
+
+    cds = [0] * (stop-start)
+
+    # fetch read from all bams mapping on tid
+    for bamfile in bamfiles:
+
+        dtid2ref = tid2ref_dict[bamfile]
+
+        if not orf_tid in dtid2ref:
+            continue
+
+        tid = dtid2ref[orf_tid]
+
+        bam_offsets = pd_offsets[pd_offsets.bam == bamfile]
+        doffsets = bam_offsets[["read_length", "offset_table_key"]].set_index('read_length').to_dict ()['offset_table_key']
+
+        bam = bam_dict[bamfile]
+        
+
+        if not orf_tid in dtid2ref:
+            print (f"ERROR: {orf_tid} not found in bam... skipping")
+            continue
+        
+        # array of p-site position counts
+        # if len(cds)%3 != 0:
+        #     print (f"ERROR: CDS-length invalid {tid}:{start}-{stop}")
+        #     continue
+
+        for read in bam.fetch (tid, start, stop):
+                    
+            read_length = read.infer_read_length () 
+            
+            if not read_length in doffsets:
+                continue
+
+            length_offset = doffsets[read_length]
+            
+            offset_pos = read.pos + length_offset
+                    
+            if offset_pos >= start and offset_pos < stop: 
+                cds[offset_pos-start] += 1
+
+
+    return (cds)
+
+def get_phasing (bamfiles, orfs, offsets, output, percentile=0.9, alpha = 0.01, devel = False):
 
     print ("### get_phasing ###")
+    
+    if not libmtspec:
+        print("module 'mtspec' is required for devel mode - setting devel = False")
+        devel = False
 
+    if devel:
+        multiplier = [.5, 1, 2]
+    else:
+        multiplier = [1]
+
+
+    bam_dict = {}
+    tid2ref_dict = {}
+    
     print ("loading bam...")
     
-    bam = pysam.Samfile (bamfile)
+    for bamfile in bamfiles:
 
-    dtid2count, dtid2ref = get_tid_info (bamfile)
+        bam_dict[bamfile] = pysam.Samfile (bamfile)
 
+        _, tid2ref = get_tid_info (bamfile)
+        tid2ref_dict[bamfile] = tid2ref
+
+
+    
     print ("loading offsets...")
 
     pd_offsets = pd.read_csv (offsets, sep="\t")
+
+    # only use read_length showing significant phasing
+    
+    pd_offsets = pd_offsets[(pd_offsets.offset_table_pct >= percentile) & 
+                            (pd_offsets.p_wilcox_percentile <= alpha) & 
+                            (pd_offsets.p_binom_percentile <= alpha)]
+
 
     header = {}
 
@@ -83,51 +210,123 @@ def get_phasing (bamfile, orfs, offsets, output):
                     header[col] = icol
 
                 # printing output header
-                print ("\t".join (columns + ["frame0", "frame1", "frame2", "p_wilcox", "p_binom"]), file=fout)
-                continue
-                
-            bam_offsets = pd_offsets[pd_offsets.bam == bamfile]
-            doffsets = bam_offsets[["read_length", "offset"]].set_index('read_length').to_dict ()['offset']
-            
-            tid, start, stop = dtid2ref[columns[header['tid']]], int(columns[header['start']]), int(columns[header['stop']])+3
 
-            # print (tid, file=sys.stderr)
+                header_col = columns + ["total_counts", "frame0", "frame1", "frame2"]
+
+                stats = ["p_wilcox", "p_binom", "p_glm"]
+                
+                if devel:
+                    stats += ["p_taper"]
+
+                for f in multiplier:                    
+                    header_col += [s + "_" + str(f) if f != 1 else s for s in stats]
+                    
+
+                print ("\t".join (header_col), file=fout)
+                continue
+
+            orf_tid = columns[header['tid']] 
+            start, stop = int(columns[header['start']]), int(columns[header['stop']])+3
+
+            cds = get_psites (orf_tid, start, stop, bamfiles, pd_offsets, tid2ref_dict=tid2ref_dict, bam_dict=bam_dict)
+
+            """           
 
             cds = [0] * (stop-start)
 
-            if len(cds)%3 != 0:
-                print (f"ERROR: CDS-length invalid {tid}:{start}-{stop}")
-                continue
+            # fetch read from all bams mapping on tid
+            for bamfile in bamfiles:
 
-            for read in bam.fetch (tid, start, stop):
-                        
-                read_length = read.infer_read_length () 
-                
-                if not read_length in doffsets:
+                dtid2ref = tid2ref_dict[bamfile]
+
+                if not orf_tid in dtid2ref:
                     continue
 
-                length_offset = doffsets[read_length]
+                tid = dtid2ref[orf_tid]
+
+                bam_offsets = pd_offsets[pd_offsets.bam == bamfile]
+                doffsets = bam_offsets[["read_length", "offset_table_key"]].set_index('read_length').to_dict ()['offset_table_key']
+
+                bam = bam_dict[bamfile]
                 
-                offset_pos = read.pos + length_offset
-                        
-                if offset_pos >= start and offset_pos < stop: 
-                    cds[offset_pos-start] += 1
+
+                if not orf_tid in dtid2ref:
+                    print (f"ERROR: {orf_tid} not found in bam... skipping")
+                    continue
+                
+                # array of p-site position counts
+                
+
+                if len(cds)%3 != 0:
+                    print (f"ERROR: CDS-length invalid {tid}:{start}-{stop}")
+                    continue
+
+                for read in bam.fetch (tid, start, stop):
+                            
+                    read_length = read.infer_read_length () 
+                    
+                    if not read_length in doffsets:
+                        continue
+
+                    length_offset = doffsets[read_length]
+                    
+                    offset_pos = read.pos + length_offset
+                            
+                    if offset_pos >= start and offset_pos < stop: 
+                        cds[offset_pos-start] += 1
 
 
-            mat, wilcox_p, binom_p = get_phasing_stats (cds)
+            """
+
+            mat = get_phasing_matrix (cds)
+            output = columns + [str(sum(sum(mat))), str(sum(mat[:,0])), str(sum(mat[:,1])), str(sum(mat[:,2]))]
+
+            # the multiplier is only relevent in devel-mode to show signal-length vs statistics
+            for f in multiplier:
+                    
+                if f < 1:
+                    new_l = int(len (cds)*f)
+                    new_l -= new_l%3
+                    sub_cds = cds[0:new_l]
+                else:
+                    sub_cds = cds * int(f)
+
+                mat = get_phasing_matrix (sub_cds)
+                wilcox_p, binom_p, glm_p = get_phasing_stats (mat)
+                
+                output += [str(wilcox_p), str(binom_p), str(glm_p)]
+
+                if devel:
+                    output += [str(get_spectrum (sub_cds))]
             
-            print ("\t".join (columns + [str(sum(mat[:,0])), str(sum(mat[:,1])), str(sum(mat[:,2])), str(wilcox_p), str(binom_p)]), file=fout)
 
-    print ("done")
+            print ("\t".join (output), file=fout)
 
 
-if __name__ == "__main__":
+    print ("### Done ###")
+
+
+def ribofy_phasing ():
 
     parser = argparse.ArgumentParser(description='get phasing')
-    parser.add_argument("--bam", dest='bam', required=True, help="Bam file - sorted and indexed")
+    parser.add_argument("--bam", dest='bam', required=True, nargs="+", help="Bam file - sorted and indexed")
     parser.add_argument("--orfs",   dest='orfs', required=True, help="orfs - generated by get_ORFs.py")
     parser.add_argument("--offsets", dest='offsets', required=True, help="offsets - generated by get_offsets.py")
     parser.add_argument("--output", dest='output', default = "ribofy_phasing.txt", help="output")
+
+    parser.add_argument("--percentile", dest='percentile', default = 0.9, help="Percentile of consistent offset-determinants")
+    parser.add_argument("--alpha", dest='alpha', default = 0.01, help="cutoff p-value for phase-detection at percentile")
+
+
+
     args = parser.parse_args()
 
-    get_phasing (args.bam, args.orfs, args.offsets, args.output)
+    get_phasing (args.bam, args.orfs, args.offsets, args.output,
+                 percentile=args.percentile,
+                 alpha=args.alpha)
+
+
+
+if __name__ == "__main__":
+        
+    ribofy_phasing ()
